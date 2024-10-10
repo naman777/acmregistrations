913d794bc0df5f21589b7e1cd14f98cc9fdc2041
Naman Kundra <namankundra6@gmail.com>
2024-10-10 12:08:05 +0530

    added all features

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,78 +1,70 @@
-import React, { useState } from 'react';
-import InputField from './Inputfield';
-import SubmitButton from './SubmitButton';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Spinner } from './Spinner';
-
+import React, { useEffect, useState } from "react";
+import InputField from "./Inputfield";
+import SubmitButton from "./SubmitButton";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Spinner } from "./Spinner";
+import FaceCapture from "./FaceCapture";
 
 const ApplyNow = () => {
   const navigate = useNavigate();
-    const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFaceVerified, setIsFaceVerified] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
 
-    const departments=[
-      " Marketing",
-      " Design/Video Editing",
-      " Social Media",
-      " Content",
-      " PR and Outreach",
-      " Finance",
-      " Other"
-    ];
+  const departments = [
+    " Marketing",
+    " Design/Video Editing",
+    " Social Media",
+    " Content",
+    " PR and Outreach",
+    " Finance",
+    " Other",
+  ];
 
-    const tdepartments=[
-      "  Web Development",
-      "  App Development",
-      "  Machine Learning/AI/Data Science",
-      "  UI/UX",
-      "  Cybersecurity",
-      "  Devops",
-      "  Blockchain",
-      "  Game Development"
-    ];
-  
+  const tdepartments = [
+    "  Web Development",
+    "  App Development",
+    "  Machine Learning/AI/Data Science",
+    "  UI/UX",
+    "  Cybersecurity",
+    "  Devops",
+    "  Blockchain",
+    "  Game Development",
+  ];
 
   const [formData, setFormData] = useState({
-    name: '',
-    branch: '',
-    rollNo: '',
-    phoneNumber: '',
-    email: '',
-    ans1: '',
-    ans2: '',
-    ans3: '',
-    ans4: '',
-    techdepartments: [] ,
-    ans5: '',
-    ans6: '',
-    nontechdepartments: [] ,
-    ans7: '',
-    image: ''
+    name: "",
+    branch: "",
+    rollNo: "",
+    phoneNumber: "",
+    email: "",
+    ans1: "",
+    ans2: "",
+    ans3: "",
+    ans4: "",
+    techdepartments: [],
+    ans5: "",
+    ans6: "",
+    nontechdepartments: [],
+    ans7: "",
+    image: "",
   });
 
-  const handleImageUpload=(e)=>{
-    const file=e.target.files[0];
-    if(file){
-      const reader=new FileReader();
-      reader.onloadend=()=>{
-        setFormData({
-          ...formData,
-          image: reader.result,
-        });
-      };
-      reader.readAsDataURL(file);
-    }
-  };
   
-  const handleCheckboxChange=(e)=>{
-    const {value,checked }=e.target;
-    const updatedDepartments=checked
+  
+
+  const handleCheckboxChange = (e) => {
+    const { value, checked } = e.target;
+    const updatedDepartments = checked
       ? [...formData.nontechdepartments, value]
-      : formData.nontechdepartments.filter((department)=>department!==value);
+      : formData.nontechdepartments.filter(
+          (department) => department !== value
+        );
 
     setFormData({
       ...formData,
-      nontechdepartments: updatedDepartments
+      nontechdepartments: updatedDepartments,
     });
   };
 
@@ -84,7 +76,7 @@ const ApplyNow = () => {
 
     setFormData({
       ...formData,
-      techdepartments: updatedDepartments
+      techdepartments: updatedDepartments,
     });
   };
 
@@ -92,62 +84,129 @@ const ApplyNow = () => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
+      image: imageUrl,
     });
   };
 
   const handleSubmit = async (e) => {
-    
-
     e.preventDefault();
     try {
-        setIsLoading(true);
-      const res = await axios.post('https://acmfirstyears.acmexamportal.workers.dev/', formData);
-        setIsLoading(false);
+
+      const formDataToSend = {
+        ...formData,
+        image_url: imageUrl, // Add imageUrl here
+      };
+      setIsLoading(true);
+      const res = await axios.post(
+        "https://acmfirstyears.acmexamportal.workers.dev/",
+        formDataToSend
+      );
+      setIsLoading(false);
       if (res.status === 200) {
-        navigate('/thankyou');
+        // navigate("/thankyou");
       } else {
-        alert('Error in submitting the form');
+        alert("Error in submitting the form");
       }
     } catch (error) {
       console.error(error);
-      alert('An error occurred while submitting the form');
+      alert("An error occurred while submitting the form");
     }
   };
 
   const inputFields = [
-    { label: 'Name', type: 'text', name: 'name', placeholder: 'Enter your name' },
-    { label: 'Email', type: 'email', name: 'email', placeholder: 'Enter your email' },
-    { label: 'Branch', type: 'text', name: 'branch', placeholder: 'Enter your branch' },
-    { label: 'Roll No. (Application No.)', type: 'text', name: 'rollNo', placeholder: 'Enter your roll number' },
-    { label: 'Phone Number', type: 'tel', name: 'phoneNumber', placeholder: 'Enter your phone number' },
-    { label: 'Why do you want to join ACM ?', type: 'text', name: 'ans1', placeholder: 'Long-answer text' },
-    { label: 'What sets you apart from the other candidates who will be applying for the same spot?', type: 'text', name: 'ans2', placeholder: 'Long-answer text' },
-    { label: 'Are you involved in any other socities ?', type: 'text', name: 'ans3', placeholder: 'Long-answer text' },
-    { label: 'What is your tech stack? (Include all the coding languages plus the software that you have learnt or learning) ', type: 'text', name: 'ans4', placeholder: 'Long-answer text' },
+    {
+      label: "Name",
+      type: "text",
+      name: "name",
+      placeholder: "Enter your name",
+    },
+    {
+      label: "Email",
+      type: "email",
+      name: "email",
+      placeholder: "Enter your email",
+    },
+    {
+      label: "Branch",
+      type: "text",
+      name: "branch",
+      placeholder: "Enter your branch",
+    },
+    {
+      label: "Roll No. (Application No.)",
+      type: "text",
+      name: "rollNo",
+      placeholder: "Enter your roll number",
+    },
+    {
+      label: "Phone Number",
+      type: "tel",
+      name: "phoneNumber",
+      placeholder: "Enter your phone number",
+    },
+    {
+      label: "Why do you want to join ACM ?",
+      type: "text",
+      name: "ans1",
+      placeholder: "Long-answer text",
+    },
+    {
+      label:
+        "What sets you apart from the other candidates who will be applying for the same spot?",
+      type: "text",
+      name: "ans2",
+      placeholder: "Long-answer text",
+    },
+    {
+      label: "Are you involved in any other socities ?",
+      type: "text",
+      name: "ans3",
+      placeholder: "Long-answer text",
+    },
+    {
+      label:
+        "What is your tech stack? (Include all the coding languages plus the software that you have learnt or learning) ",
+      type: "text",
+      name: "ans4",
+      placeholder: "Long-answer text",
+    },
   ];
 
   const inputFields2 = [
-    
-    { label: 'Proof of prior work experience ?', type: 'text', name: 'ans5', placeholder: 'Long-answer text' },
-    { label: 'Extra links(Github, LinkedIn, Leetcode, etc.)', type: 'text', name: 'ans6', placeholder: 'Long-answer text' },
+    {
+      label: "Proof of prior work experience ?",
+      type: "text",
+      name: "ans5",
+      placeholder: "Long-answer text",
+    },
+    {
+      label: "Extra links(Github, LinkedIn, Leetcode, etc.)",
+      type: "text",
+      name: "ans6",
+      placeholder: "Long-answer text",
+    },
   ];
 
   const inputFields3 = [
-    
-    { label: 'Do you have any prior experience in the non tech department(s) you selected? If yes, please mention it.', type: 'text', name: 'ans7', placeholder: 'Long-answer text' },
+    {
+      label:
+        "Do you have any prior experience in the non tech department(s) you selected? If yes, please mention it.",
+      type: "text",
+      name: "ans7",
+      placeholder: "Long-answer text",
+    },
   ];
 
   return (
-    <div className='w-full'>
+    <div className="w-full">
       <div className="flex justify-center py-3 ">
         <img src="/acm.png" alt="ACM Logo" />
-    </div>
-
+      </div>
 
       <main className="flex overflow-hidden flex-col mx-auto w-full text-black bg-white max-w-[480px]">
         <section className="flex flex-col px-5 mt-0 w-full">
-          <div className='mb-20'>
+          <div className="mb-20">
             <div className="flex items-start self-center text-4xl font-bold ">
               <h1 className="grow self-end mt-10 mr-0">Apply Now</h1>
             </div>
@@ -155,7 +214,10 @@ const ApplyNow = () => {
               <div className="shrink-0 self-start mt-4 w-14 border-2 border-black border-solid h-[5px] bg-black" />
               <div className="basis-auto">Register with</div>
             </div>
-
+            <FaceCapture
+                  setImageUrl={setImageUrl}
+                  setIsFaceVerified={setIsFaceVerified}
+                />
             <form onSubmit={handleSubmit}>
               {inputFields.map((field, index) => (
                 <InputField
@@ -168,11 +230,16 @@ const ApplyNow = () => {
                   name={field.name} // Add this line to pass the name prop
                 />
               ))}
-              
+
               <div className="mt-6">
-                <h3 className="text-base font-semibold">Which tech department(s) interest you?</h3>
+                <h3 className="text-base font-semibold">
+                  Which tech department(s) interest you?
+                </h3>
                 {tdepartments.map((department, index) => (
-                  <label key={index} className="block px-3 py-2 mt-2.5 text-sm tracking-normal leading-none rounded-md border border-gray-200 border-solid bg-slate-50 w-full">
+                  <label
+                    key={index}
+                    className="block px-3 py-2 mt-2.5 text-sm tracking-normal leading-none rounded-md border border-gray-200 border-solid bg-slate-50 w-full"
+                  >
                     <input
                       type="checkbox"
                       value={department}
@@ -184,7 +251,6 @@ const ApplyNow = () => {
                 ))}
               </div>
 
-            
               {inputFields2.map((field, index) => (
                 <InputField
                   key={index}
@@ -198,9 +264,14 @@ const ApplyNow = () => {
               ))}
 
               <div className="mt-6">
-                <h3 className="text-base font-semibold">Which non-tech department(s) interest you?</h3>
+                <h3 className="text-base font-semibold">
+                  Which non-tech department(s) interest you?
+                </h3>
                 {departments.map((department, index) => (
-                  <label key={index} className="block px-3 py-2 mt-2.5 text-sm tracking-normal leading-none rounded-md border border-gray-200 border-solid bg-slate-50 w-full">
+                  <label
+                    key={index}
+                    className="block px-3 py-2 mt-2.5 text-sm tracking-normal leading-none rounded-md border border-gray-200 border-solid bg-slate-50 w-full"
+                  >
                     <input
                       type="checkbox"
                       value={department}
@@ -212,30 +283,21 @@ const ApplyNow = () => {
                 ))}
 
                 {inputFields3.map((field, index) => (
-                <InputField
-                  key={index}
-                  label={field.label}
-                  type={field.type}
-                  placeholder={field.placeholder}
-                  value={formData[field.name]}
-                  onChange={handleChange}
-                  name={field.name} // Add this line to pass the name prop
-                />
-              ))}
-              <div className="mt-6">
-                <h3 className="text-base font-semibold">Upload your Image.. (Keep in mind your face should be visible clearly , preferably a selfie)</h3>
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={handleImageUpload}
-                  className="block px-3 py-2 mt-2.5 text-sm tracking-normal leading-none rounded-md border border-gray-200 border-solid bg-slate-50 w-full"
-                />
+                  <InputField
+                    key={index}
+                    label={field.label}
+                    type={field.type}
+                    placeholder={field.placeholder}
+                    value={formData[field.name]}
+                    onChange={handleChange}
+                    name={field.name} // Add this line to pass the name prop
+                  />
+                ))}
+                
               </div>
-              </div>
-              
-              <SubmitButton />
+              {isFaceVerified ? <SubmitButton /> : <p>Please verify your face to proceed.</p>}
+
             </form>
-            
           </div>
         </section>
       </main>
--- src/pages/SubmitButton.jsx
+++ src/pages/SubmitButton.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const SubmitButton = ({onclick}) => {
+const SubmitButton = ({ onClick }) => {
   return (
     <button
       type="submit"
-      className="flex gap-2 self-center px-9 py-4 mt-8 max-w-full text-xl font-semibold text-white whitespace-nowrap bg-sky-500 rounded-2xl shadow-[0px_8px_23px_rgba(173,220,255,1)] w-[180px]" onclick={onclick}
+      className="flex gap-2 self-center px-9 py-4 mt-8 max-w-full text-xl font-semibold text-white whitespace-nowrap bg-sky-500 rounded-2xl shadow-[0px_8px_23px_rgba(173,220,255,1)] w-[180px]"
+      onClick={onClick} // Change onclick to onClick
     >
       <span className="grow">Submit</span>
-      <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/1b3f218f4269107c990a1fffcd133ff33dbba696fed536ec177ac34a3f8adbee?placeholderIfAbsent=true&apiKey=3bcad2a00ff743a3a851fc72d0289ec0" alt="" className="object-contain shrink-0 my-auto aspect-square w-[18px]" />
+      <img
+        loading="lazy"
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/1b3f218f4269107c990a1fffcd133ff33dbba696fed536ec177ac34a3f8adbee?placeholderIfAbsent=true&apiKey=3bcad2a00ff743a3a851fc72d0289ec0"
+        alt=""
+        className="object-contain shrink-0 my-auto aspect-square w-[18px]"
+      />
     </button>
   );
 };
